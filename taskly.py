2b1c2b1fea4b14dbd5a23caa4751a84169f83765
Mwasigwa2 <gathirimu.mwasigwa@student.moringaschool.com>
2023-09-08 04:42:10 +0300

    Base 2

--- taskly.py
+++ taskly.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-# taskly.py
 import click
 from colorama import Fore, Style
+import time
+
+# Our dictionary to store task timers
+task_timers = {}
 
 @click.command()
 def taskly():
@@ -10,23 +13,31 @@ def taskly():
     click.echo(Fore.GREEN + "Taskly - Your Task Manager with in-built Timer")
     click.echo(Style.RESET_ALL)
 
-@taskly.command()
+
 @click.argument("task_name")
 def add(task_name):
     """Add a new task"""
     # Implement task addition logic here
     click.echo(f"Added task: {task_name}")
 
-@taskly.command()
-def list():
+def show():
     """List all tasks"""
     # Implement task listing logic here
     click.echo("List of tasks:")
 
-# Add more commands as needed
+# Timer
+
+
+@click.argument("task_name")
+def stop(task_name):
+    """Stop a task timer"""
+    if task_name in task_timers:
+        elapsed_time = time.time() - task_timers[task_name]
+        click.echo(f"Stopped timer for task: {task_name}")
+        click.echo(f"Elapsed time: {elapsed_time:.2f} seconds")
+        del task_timers[task_name]
+    else:
+        click.echo(f"Task '{task_name}' timer is not running.")
 
 if __name__ == "__main__":
     taskly()
-
-
-
